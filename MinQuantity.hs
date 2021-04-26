02ee43bd519adde3fb396a610825cbaabda4debe
H@di <hadi@hadisafari.ir>
2021-04-27 01:35:57 +0430

    bypass already rejected orders in minQuantityCheck

--- MinQuantity.hs
+++ MinQuantity.hs
@@ -1,22 +1,20 @@
 module MinQuantity where
 
-import Data.Map
 import Coverage
 import ME
 
 minQuantityCheck :: Decorator
-minQuantityCheck handler =
-  \rq s ->
-    case rq of
-      (NewOrderRq o) -> do
-        { (rs, s') <- handler rq s
-        ; case minQty o of
-            Nothing -> (rs, s') `covers` "MQC1"
-            Just mq ->
-              if (sum $ Prelude.map quantityTraded $ trades rs) >= mq then
-                (rs, s') `covers` "MQC2"
-              else
-                (NewOrderRs Rejected [], s) `covers` "MQC3"
-        }
-      _ -> handler rq s
+minQuantityCheck handler rq s = case rq of
+  (NewOrderRq o) -> do
+    (rs, s') <- handler rq s
+    case status rs of
+      Accepted -> case minQty o of
+        Nothing -> (rs, s') `covers` "MQC1"
+        Just mq
+          -> if sum (Prelude.map quantityTraded $ trades rs) >= mq then
+              (rs, s') `covers` "MQC2"
+            else
+              (NewOrderRs Rejected [], s) `covers` "MQC3"
+      Rejected -> (rs, s') `covers` "MQC4"
+  _ -> handler rq s
 
