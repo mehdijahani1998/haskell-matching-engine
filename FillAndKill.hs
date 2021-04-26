9dfe05cd905eab3c07d00b32d4626951dded936d
H@di <hadi@hadisafari.ir>
2021-04-27 01:38:35 +0430

    bypass already rejected orders in fillAndKillProc

--- FillAndKill.hs
+++ FillAndKill.hs
@@ -1,6 +1,5 @@
 module FillAndKill where
 
-import Data.Map
 import Coverage
 import ME
 
@@ -9,15 +8,15 @@ removeOrderFromState o s =
   s { orderBook = removeOrderFromOrderBook o (orderBook s) }
 
 fillAndKillProc :: Decorator
-fillAndKillProc handler =
-  \rq s ->
-    case rq of
-      (NewOrderRq o) -> do
-        { (rs, s') <- handler rq s
-        ; if fillAndKill o then
-            (rs, removeOrderFromState o s') `covers` "FKP1"
-          else
-            (rs, s') `covers` "FKP2"
-        }
-      _ -> handler rq s
+fillAndKillProc handler rq s = case rq of
+  (NewOrderRq o) -> do
+    (rs, s') <- handler rq s
+    case status rs of
+      Accepted -> 
+        if fillAndKill o then
+          (rs, removeOrderFromState o s') `covers` "FKP1"
+        else
+          (rs, s') `covers` "FKP2"
+      Rejected -> (rs, s') `covers`  "FKP3"
+  _ -> handler rq s
 
