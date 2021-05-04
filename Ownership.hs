8663bf3ba911fc2da9250f8976f80e215c3e4749
H@di <hadi@hadisafari.ir>
2021-05-04 16:39:54 +0430

    complete ownership on cancel and replace

--- Ownership.hs
+++ Ownership.hs
@@ -9,17 +9,22 @@ ownershipCheck maxOwnership handler rq s = case rq of
   (NewOrderRq o) -> do  
     (rs, s') <- handler rq s
     case status rs of
-      Accepted ->  if ownershipPreCheck maxOwnership o s then do
+      Accepted ->  if ownershipPreCheck maxOwnership o Nothing s then do
           (rs, updateOwnershipInfo (trades rs) s') `covers` "OSC1"
         else
           (NewOrderRs Rejected [], s) `covers` "OSC2"
       Rejected -> (rs, s') `covers` "OSC3"
-  (CancelOrderRq rqid oid side) -> do -- FIXME
+  (CancelOrderRq rqid oid side) -> do
     (rs, s') <- handler rq s
     (rs, s') `covers` "OSC4"
-  (ReplaceOrderRq oldoid o) -> do -- FIXME
+  (ReplaceOrderRq oldoid o) -> do
     (rs, s') <- handler rq s
-    (rs, s') `covers` "OSC5"
+    case status rs of
+      Accepted ->  if ownershipPreCheck maxOwnership o (oldOrder rs) s then do
+          (rs, updateOwnershipInfo (trades rs) s') `covers` "OSC5"
+        else
+          (ReplaceOrderRs Rejected Nothing [], s) `covers` "OSC6"
+      Rejected -> (rs, s') `covers` "OSC7"
   _ -> handler rq s
 
 updateOwnershipInfo :: [Trade] -> MEState -> MEState
@@ -28,16 +33,26 @@ updateOwnershipInfo ts (MEState ob ci oi) =
    
 updateOwnership :: OwnershipInfo -> Trade -> OwnershipInfo
 updateOwnership oi t =
-  adjust (+ q) bshid $ adjust (\v -> v - q) sshid oi
+  adjust (+ q) bshid $ adjust (subtract q) sshid oi
   where
     q = quantityTraded t
     bshid = buyerShId t
-    sshid = sellerShId t  
+    sshid = sellerShId t
 
-ownershipPreCheck :: Int -> Order -> MEState -> Bool
-ownershipPreCheck maxOwnership o (MEState ob _ ownership) =
+quantityInQueue :: Maybe Order -> OrderBook -> Int
+quantityInQueue (Just o) ob =
+  sum $ 
+  Prelude.map quantity $
+  Prelude.filter (\orderInQueue -> oid orderInQueue == oid o) $
+  (if side o == Buy then buyQueue else sellQueue) $
+  ob
+
+quantityInQueue Nothing ob = 0
+
+ownershipPreCheck :: Int -> Order -> Maybe Order -> MEState -> Bool
+ownershipPreCheck maxOwnership o oldOrder (MEState ob _ ownership) =
   case side o of
-    Buy -> (ownership!shi) + (quantity o) + (totalQuantity Buy shi ob) < maxOwnership
-    Sell -> (quantity o) + (totalQuantity Sell shi ob) <= (ownership!shi)
+    Buy -> (ownership!shi) + (quantity o) + (totalQuantity Buy shi ob) - (quantityInQueue oldOrder ob) < maxOwnership
+    Sell -> (quantity o) + (totalQuantity Sell shi ob) - (quantityInQueue oldOrder ob) <= (ownership!shi)
   where
     shi = shid o
