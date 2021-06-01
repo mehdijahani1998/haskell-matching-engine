84b15513822cc87d13fb6030a9665aed069ea60f
H@di <hadi@hadisafari.ir>
2021-06-01 17:13:31 +0430

    rename test files

--- test.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-cabal build GetTCTraces &&
-./dist/build/GetTCTraces/GetTCTraces --trades test/test1.txt > test/sink &&
-diff -U -1 test/test1_res.txt test/sink | colordiff | diff-highlight
