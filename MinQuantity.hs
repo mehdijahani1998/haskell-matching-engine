d368479a0ff91d3ffb7c760a79d6cc95fa47856a
H@di <hadi@hadisafari.ir>
2021-09-21 12:32:20 +0430

    fix name of methods

--- MinQuantity.hs
+++ MinQuantity.hs
@@ -7,16 +7,16 @@ import           ME
 
 minQuantityCheck :: Decorator
 minQuantityCheck =
-    decorateOnAccept "MQC" pricebandCheckByType
+    decorateOnAccept "MQC" minQuantityCheckByType
 
 
-pricebandCheckByType :: PartialDecorator
-pricebandCheckByType rq@(NewOrderRq o) s rs =
+minQuantityCheckByType :: PartialDecorator
+minQuantityCheckByType rq@(NewOrderRq o) s rs =
     case minQty o of
         Nothing -> rs `covers` "MQC1"
         Just mq -> if sum (Prelude.map quantityTraded $ trades rs) >= mq
             then rs `covers` "MQC2"
             else (reject rq s) { status = Eliminated } `covers` "MQC3"
 
-pricebandCheckByType _ _ rs =
+minQuantityCheckByType _ _ rs =
     rs `covers`  "MQC-P"
